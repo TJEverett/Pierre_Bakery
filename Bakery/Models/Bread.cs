6a19cbb8960598c46cdccd2a561b3ef44d9351fd
Tristen Everett <spock214@gmail.com>
2020-10-07 09:56:04 -0700

    rename file and split logic

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -21,24 +21,4 @@ namespace Bakery
       Price += (notCounted / 3) * 10;
     }
   }
-
-  public class Pastry
-  {
-    public int Count;
-    public int Price;
-
-    public Pastry(int amount)
-    {
-      Count = amount;
-    }
-
-    public void CalculatePrice()
-    {
-      Price = 0;
-      int notCounted = Count;
-      Price += (notCounted % 3) * 2;
-      notCounted -= notCounted % 3;
-      Price += (notCounted / 3) * 5;
-    }
-  }
 }
---
+++ Bakery/Models/Pastries.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Bakery
+{
+  public class Pastry
+  {
+    public int Count;
+    public int Price;
+
+    public Pastry(int amount)
+    {
+      Count = amount;
+    }
+
+    public void CalculatePrice()
+    {
+      Price = 0;
+      int notCounted = Count;
+      Price += (notCounted % 3) * 2;
+      notCounted -= notCounted % 3;
+      Price += (notCounted / 3) * 5;
+    }
+  }
+}
